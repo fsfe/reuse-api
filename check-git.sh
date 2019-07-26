e890f0f1a9a324c064eaf92c9ca391f153450306
max.mehl <max.mehl@fsfe.org>
2019-07-26 21:39:01 +0200

    ansible playbook to set up a server executing the reuse lint commands inside the reuse-api docker image

--- check-git.sh
+++ check-git.sh
@@ -2,8 +2,10 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright © 2019 Free Software Foundation Europe e.V.
 
+# Git repo
 GIT=$1
-OPTIONS=$2
+# All further options from ENV variable, provided to docker
+OPTIONS="$REUSE_OPT"
 
 # Test if remote repository is valid
 if ! git ls-remote "${GIT}" > /dev/null; then
