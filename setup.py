2865d5d99edc6739eb53b4fe1762f3e9f5fd1721
Carmen Bianca Bakker <carmenbianca@fsfe.org>
2019-08-28 10:41:34 +0200

    Replace SPDX-Copyright with SPDX-FileCopyrightText

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# SPDX-Copyright: 2019 Free Software Foundation Europe e.V.
+# SPDX-FileCopyrightText: 2019 Free Software Foundation Europe e.V.
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
