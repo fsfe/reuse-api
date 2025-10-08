1c24e05aa7bc6b224cfff60377e6733b11bd890b
Filip Kobierski <fkobi@fsfe.org>
2025-10-08 17:05:10 +0200

    reuse_api/config.py: migrate from os.environ.net to getenv
    Signed-off-by: Filip Kobierski <fkobi@fsfe.org>

--- reuse_api/config.py
+++ reuse_api/config.py
@@ -4,48 +4,48 @@
 
 """Configuration for the REUSE API service"""
 
-import os
+from os import getenv, path
 
 
 # Configuration for Flask
-SECRET_KEY: str = os.environ.get("SECRET_KEY", "secret_key")
+SECRET_KEY: str = getenv("SECRET_KEY", default="secret_key")
 
 # Admin key for the REUSE API, necessary for some operations
-ADMIN_KEY: str = os.environ.get("ADMIN_KEY", "admin_key")
+ADMIN_KEY: str = getenv("ADMIN_KEY", default="admin_key")
 
 # Configuration for the repository storage
 # NOTE: A relative path currently (Flask-SQLAlchemy < 3.0) is relative to the
 # application root, not relative to the current working directory! So we have
 # to define an absolute path.
 # See also https://github.com/pallets/flask-sqlalchemy/issues/462
-SQLALCHEMY_DATABASE_URI: str = os.environ.get(
-    "SQLALCHEMY_DATABASE_URI", "sqlite:///:memory:"
+SQLALCHEMY_DATABASE_URI: str = getenv(
+    "SQLALCHEMY_DATABASE_URI", default="sqlite:///:memory:"
 )
 SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
 
 # Configuration for the form server used for registration
-FORMS_URL: str = os.environ.get("FORMS_URL", "https://forms.fsfe.org/email")
-FORMS_FILE: str = os.environ.get("FORMS_FILE", "repos.json")
-if not os.path.isfile(FORMS_FILE):
+FORMS_URL: str = getenv("FORMS_URL", default="https://forms.fsfe.org/email")
+FORMS_FILE: str = getenv("FORMS_FILE", default="repos.json")
+if not path.isfile(FORMS_FILE):
     raise FileNotFoundError(
         "Can't run without access to the '%s' file", FORMS_FILE
     )
 
 # SSH configurations
-SSH_KEY_PATH: str = os.environ.get("SSH_KEY_PATH", "~/.ssh/reuse_ed25519")
-SSH_KNOW_HOST_PATH: str = os.environ.get(
-    "SSH_KNOW_HOST_PATH", "~/.ssh/known_hosts"
+SSH_KEY_PATH: str = getenv("SSH_KEY_PATH", default="~/.ssh/reuse_ed25519")
+SSH_KNOW_HOST_PATH: str = getenv(
+    "SSH_KNOW_HOST_PATH", default="~/.ssh/known_hosts"
 )
-SSH_PORT: int = int(os.environ.get("SSH_PORT", "22"))
-SSH_USER: str = os.environ.get("SSH_USER", "reuse")
+SSH_PORT: int = int(getenv("SSH_PORT", default=22))
+SSH_USER: str = getenv("SSH_USER", "reuse")
 
 # Servername
-REUSE_API: str = os.environ.get("REUSE_API", "wrk1.api.reuse.software")
+REUSE_API: str = getenv("REUSE_API", "wrk1.api.reuse.software")
 
 # Number of maximum checks in queue
-NB_RUNNER: int = int(os.environ.get("NB_RUNNER", "6"))
+NB_RUNNER: int = int(getenv("NB_RUNNER", default=6))
 
 # Number of repository return during pagination
 NB_REPOSITORY_BY_PAGINATION: int = int(
-    os.environ.get("NB_REPOSITORY_BY_PAGES", "10")
+    getenv("NB_REPOSITORY_BY_PAGES", default=10)
 )
