c9b146f0384b0771eaeb9138d494334686f2f611
Filip Kobierski <fkobi@fsfe.org>
2025-12-12 11:04:34 +0100

    views: rewire /

--- reuse_api/views.py
+++ reuse_api/views.py
@@ -36,7 +36,7 @@ json_blueprint = Blueprint("json", __name__)
 
 @html_blueprint.route("/")
 def index() -> str:
-    return render_template("index.jinja2", compliant_repos=Repository.projects().total)
+    return render_template("index.jinja2", compliant_repos=len(getall()))
 
 
 @html_blueprint.route("/register", methods=["GET", "POST"])
