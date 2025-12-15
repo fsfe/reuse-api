ed65a184081d5bf08b2a4bc823ec4f4437dc88db
Filip Kobierski <fkobi@fsfe.org>
2025-12-15 15:36:04 +0100

    views/badge: introduce get_status function

--- reuse_api/views.py
+++ reuse_api/views.py
@@ -122,13 +122,15 @@ def register() -> str:
 @html_blueprint.route("/badge/<path:url>")
 def badge(url: str) -> str:
     """The SVG badge for a repo"""
-    status: str = "unregistered"
-    row = schedule_if_new_or_later(url, current_app.scheduler)
 
-    if row is not None:
-        status = row.status
+    def get_status() -> str:
+        if not Repository.is_registered(url):
+            return "unregistered"
+        if not Repository.is_initialised(url):
+            return "uninitialised"
+        return "compliant" if Repository.is_compliant(url) else "non-compliant"
 
-    result = send_file(f"badges/{status}.svg", mimetype="image/svg+xml")
+    result = send_file(f"badges/{get_status()}.svg", mimetype="image/svg+xml")
 
     # Disable caching for badge files
     result.cache_control.max_age = 0
