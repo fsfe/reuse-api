8d19122330c4f0a8d3a67da2784000db2cc88aa6
Filip Kobierski <fkobi@fsfe.org>
2025-12-17 14:01:09 +0100

    views: rewire badge
    Signed-off-by: Filip Kobierski <fkobi@fsfe.org>

--- reuse_api/views.py
+++ reuse_api/views.py
@@ -20,7 +20,7 @@ from flask import (
 from requests import post
 from werkzeug.exceptions import HTTPException
 
-from reuse_api import models as db
+from reuse_api import db
 from reuse_api.forms import RegisterForm
 
 from .config import ADMIN_KEY, FORMS_URL
