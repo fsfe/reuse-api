602a3f7be1f74c2eee99c392eb43dab272bea885
Filip Kobierski <fkobi@fsfe.org>
2025-12-17 18:01:29 +0100

    views/register: define url locally
    Signed-off-by: Filip Kobierski <fkobi@fsfe.org>

--- reuse_api/views.py
+++ reuse_api/views.py
@@ -49,6 +49,7 @@ def register() -> str:
         form.project.data = request.args.get("url")
 
     if form.validate_on_submit():
+        url: str = form.project.data
         if FORMS_DISABLE:
             db.register(form.project.data)
         else:  # normal operation
@@ -61,7 +62,7 @@ def register() -> str:
             )
             if not response.ok:
                 return response.text, response.status_code
-        return render_template("register-success.jinja2", project=form.project.data)
+        return render_template("register-success.jinja2", project=url)
     return render_template("register.jinja2", form=form)
 
 
