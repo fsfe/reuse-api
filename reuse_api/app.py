796b0d44ae2b7b9223a284bdbc50b7a3f0bfc5c4
Filip Kobierski <fkobi@fsfe.org>
2026-01-19 14:20:47 +0100

    app: document steps
    Signed-off-by: Filip Kobierski <fkobi@fsfe.org>

--- reuse_api/app.py
+++ reuse_api/app.py
@@ -18,38 +18,38 @@ from .scheduler import Scheduler
 
 
 def create_app() -> Flask:
+    # Disable git prompt
+    environ["GIT_TERMINAL_PROMPT"] = "0"
+
+    # Configure logging format
+    logging.basicConfig(format="[%(asctime)s] %(levelname)s: %(message)s")
+
+    # Perform sanity checks
     if not access(config.FORMS_FILE, R_OK):
         raise PermissionError(
             "FORMS_FILE is not readable:", path.abspath(config.FORMS_FILE)
         )
 
-    # create and configure the app
+    # Create and configure the app
     app: Flask = Flask(__name__.split(".")[0])
     app.config.from_object(config)
-
-    # TODO: Make this configurable
-    logging.basicConfig(format="[%(asctime)s] %(levelname)s: %(message)s")
-
     app.logger.setLevel(logging.DEBUG)
 
-    app.logger.debug("Running config: %s", app.config)
+    app.register_blueprint(html_blueprint)
+    app.register_blueprint(json_blueprint)
 
-    environ["GIT_TERMINAL_PROMPT"] = "0"
+    app.logger.debug("Running config: %s", app.config)
 
     # Initialize database
     db.init_app(app)
     with app.app_context():
         db.create_all()
 
+    # Initialize scheduler
     app.scheduler = Scheduler(app)
     # FIXME: This is ideally only run when the app is fully "started", but I
     # can't find documentation for this.
     app.scheduler.run()
-
     atexit_register(app.scheduler.join)
-
-    # Register blueprints
-    app.register_blueprint(html_blueprint)
-    app.register_blueprint(json_blueprint)
 
     return app
