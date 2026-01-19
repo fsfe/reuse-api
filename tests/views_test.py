82c22ab23d22628c2cda81f3db3db7d36d7ce665
Filip Kobierski <fkobi@fsfe.org>
2026-01-19 17:06:56 +0100

    add views_test
    Signed-off-by: Filip Kobierski <fkobi@fsfe.org>

---
+++ tests/views_test.py
@@ -0,0 +1,59 @@
+from http import HTTPStatus
+
+from reuse_api.db import register, update
+
+from .conftest import TEST_REPO
+
+
+def test_gets(client) -> None:
+    """Tests simple GET requests."""
+    for p in ("/", "/projects", "/register"):
+        response = client.get(p)
+        assert response.status_code == HTTPStatus.OK, f"GET ON {p} is not OK"
+
+
+def test_info_and_badge_and_json(client, db_empty) -> None:
+    """General endpoint test"""
+    url: str = f"/info/{TEST_REPO}"
+
+    # Unregistered
+    response = client.get(url)
+    assert (
+        response.status_code == HTTPStatus.NOT_FOUND
+    ), "Unregistered info is not NOT_FOUND"
+
+    response = client.get(f"/badge/{TEST_REPO}")
+    assert response.status_code == HTTPStatus.OK, "Unregistered badge is not OK"
+
+    response = client.get(f"/status/{TEST_REPO}")
+    assert (
+        response.status_code == HTTPStatus.NOT_FOUND
+    ), f"Uninitialised json is not NOT_FOUND ({response.status_code})"
+
+    register(TEST_REPO)  # Uninitialised
+
+    response = client.get(url)
+    assert (
+        response.status_code == HTTPStatus.FAILED_DEPENDENCY
+    ), "Uninitialised info is not FAILED_DEPENDENCY"
+
+    response = client.get(f"/badge/{TEST_REPO}")
+    assert response.status_code == HTTPStatus.OK, "Uninitialised badge is not OK"
+
+    update(TEST_REPO)  # Updated
+
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK, "Updated info is not OK"
+
+    response = client.get(f"/badge/{TEST_REPO}")
+    assert response.status_code == HTTPStatus.OK, "Updated badge is not OK"
+
+    response = client.get(f"/status/{TEST_REPO}")
+    assert (
+        response.status_code == HTTPStatus.OK
+    ), f"Updated json is not OK ({response.status_code})"
+
+
+def test_sbom(client, db_updated) -> None:
+    response = client.get(f"/sbom/{TEST_REPO}.spdx")
+    assert response.status_code == HTTPStatus.OK, "Updated SBOM is not OK"
