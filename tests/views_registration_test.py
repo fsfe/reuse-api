27e54bfd3e3a9e1d11e460f640aa756e65020b7f
Filip Kobierski <fkobi@fsfe.org>
2026-01-21 13:01:26 +0100

    tests: add views_registration_test.py
    Signed-off-by: Filip Kobierski <fkobi@fsfe.org>

---
+++ tests/views_registration_test.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: 2025 Free Software Foundation Europe e.V.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from http import HTTPStatus
+
+
+from reuse_api.db import is_registered, register
+
+from .conftest import TEST_REPO
+
+
+def gendata(url: str, email: str = "my_email@fsfe.org", name="some_name") -> dict:
+    """Generates POST data contents for testing."""
+    return {"project": url, "confirm": email, "name": name}
+
+
+def test_registration_empty_post(client) -> None:
+    response = client.post("/register", data={})
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_registration_without_forms(client, db_empty) -> None:
+    assert not is_registered(TEST_REPO)
+    response = client.post("/register", data=gendata(TEST_REPO))
+    assert response.status_code == HTTPStatus.ACCEPTED
+    assert is_registered(TEST_REPO)
